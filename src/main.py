e8901f435ee8dc9949cc3517061c0bc8efcb0334
BeLEEU <35514614+BeLEEU@users.noreply.github.com>
2025-03-13 16:08:37 +0800

    可完成md格式转换为pptx格式

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 
+from template_manager import load_template, print_layouts, get_layout_mapping
+from input_parser import parse_input_text
+from ppt_generator import generate_presentation
 input_text = """
 # 人类历史之光
 
@@ -14,7 +17,8 @@ input_text = """
 ![名人肖像](images/farmous.png)
 """
 
-if __name__ == '__main__':
+def main():
+
     template_file = 'templates/Template.pptx'
     prs = load_template(template_file)
 
@@ -22,6 +26,15 @@ if __name__ == '__main__':
     print_layouts(prs)
 
     layout_mapping = get_layout_mapping(prs)
+
+    powerpoint_data, presentation_title = parse_input_text(input_text, layout_mapping)
+
+    output_pptx = f"outputs/{presentation_title}.pptx"
+    generate_presentation(powerpoint_data, template_file, output_pptx)
+
+if __name__ == '__main__':
+    main()
+
 
 
 

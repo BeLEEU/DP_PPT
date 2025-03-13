96daccd6fb09746d8a4ac05127db629b0a61554e
BeLEEU <35514614+BeLEEU@users.noreply.github.com>
2025-03-13 15:00:26 +0800

    确定ppt内容格式(markdown格式)

---
+++ src/main.py
@@ -0,0 +1,21 @@
+
+input_text = """
+# 人类历史之光
+
+## 人类历史之光 [Title Only]
+
+## 历史十大人物 [Title and Content]
+- xxx
+- xxx
+
+## 近代史十大人物 [Title and Content]
+- xxx
+- xxx
+![名人肖像](images/farmous.png)
+"""
+
+if __name__ == '__main__':
+    template_file = 'templates/Template.pptx'
+
+
+
